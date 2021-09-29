c1f5423ef66a2b1f54227998c3766d287ba8da45
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-09-29 10:35:45 +0800

    Add project files.

---
+++ W4D3_Training_Ses1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4D3_Training_Ses1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Method1(4);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught");
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static void Method1(int a)
+
+        {
+            try
+            {
+                Method2(a);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("exception caught in method1");
+                throw;
+            }
+        }
+
+        static void Method2(int a)
+        {
+            try
+            {
+                var res = a / 0;
+
+            }
+
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        
+    }
+}
